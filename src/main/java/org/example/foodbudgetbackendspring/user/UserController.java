824a0c61a2b12fe534adc9da112ec2f6c31298e4
WiktorGruszczynski <wgwiktorwg@gmail.com>
2026-03-26 11:42:54 +0100

    feat: user authentication

--- src/main/java/org/example/foodbudgetbackendspring/user/UserController.java
+++ src/main/java/org/example/foodbudgetbackendspring/user/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
 
     @PostMapping("/register")
