b25a23aa9b96d294104acd40876f72abbffb62a8
WiktorGruszczynski <wgwiktorwg@gmail.com>
2026-03-30 22:43:46 +0200

    feat: add custom user details

--- src/main/java/org/example/foodbudgetbackendspring/user/service/UserDetailsServiceImpl.java
+++ src/main/java/org/example/foodbudgetbackendspring/user/service/UserDetailsServiceImpl.java
@@ -2,27 +2,36 @@ package org.example.foodbudgetbackendspring.user.service;
 
 import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
+import org.example.foodbudgetbackendspring.user.model.CustomUserDetails;
+import org.example.foodbudgetbackendspring.user.model.User;
 import org.example.foodbudgetbackendspring.user.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-
     private final UserRepository userRepository;
 
     @Override
     @Nonnull
-    public UserDetails loadUserByUsername(@Nonnull String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
-                .map(user -> org.springframework.security.core.userdetails.User
-                        .withUsername(user.getEmail())
-                        .password(user.getPassword())
-                        .roles(user.getRole().name())
-                        .build()
-                ).orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+    public UserDetails loadUserByUsername(@Nonnull String username) {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                List.of(
+                        new SimpleGrantedAuthority("ROLE_" + user.getRole())
+                )
+        );
     }
 }
